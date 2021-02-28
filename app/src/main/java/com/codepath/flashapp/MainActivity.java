67a18d8e592d62b5dca48d5279e73b3073303b79
abhiraj-chatterjee <abhirajchatterjee@gmail.com>
2021-02-28 05:37:25 +0530

    Initial commit

---
+++ app/src/main/java/com/codepath/flashapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.codepath.flashapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
